499aa6528cb148b447e3a558ca11d048115b8e83
joseluisgs <joseluisgonsan@gmail.com>
2020-09-21 20:02:13 +0200

    Opciones nuevas de lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,23 @@ module.exports = {
     node: true,
   },
   extends: [
+    'eslint:recommended',
     'airbnb-base',
   ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2019,
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'max-len': ['error', { code: 200 }],
+    'no-param-reassign': ['error', { props: false }],
+    'max-len': [
+      'error',
+      {
+        code: 180,
+        comments: 180,
+      },
+    ],
   },
 };
