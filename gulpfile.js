f5323c675bfd3cdde02db5aa1cc3e5de29ee0192
joseluisgs <joseluisgonsan@gmail.com>
2020-09-23 19:17:39 +0200

    Gulp sigle quotes

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,50 @@
+/* eslint-disable implicit-arrow-linebreak */
 // Fichero de Gulp
-const gulp = require("gulp"); // Requerimos Gulp
-const browserSync = require("browser-sync").create(); // Refrescamos el navegador
-const sass = require("gulp-sass"); // Convertimos Sass
-const minify = require("gulp-minify");
+const gulp = require('gulp'); // Requerimos Gulp
+const browserSync = require('browser-sync').create(); // Refrescamos el navegador
+const sass = require('gulp-sass'); // Convertimos Sass
+const minify = require('gulp-minify');
 
 // Tarea para crear los ficheros Sass
-gulp.task("sass", () => {
+gulp.task('sass', () =>
   // Buscamos los fihceros fuentes de Bootstrap originales y los nuestros
-  return (
+  (
     gulp
       .src([
         // "node_modules/bootstrap/scss/bootstrap.scss",
-        "src/scss/*.scss",
+        'src/scss/*.scss',
       ])
       // Lo sacamos comprimidos en la carpeta src/css
-      .pipe(sass({ outputStyle: "compressed" }))
-      .pipe(gulp.dest("src/css"))
+      .pipe(sass({ outputStyle: 'compressed' }))
+      .pipe(gulp.dest('src/css'))
       .pipe(browserSync.stream())
-  );
-});
+  ));
 
 // Tareas para js
-gulp.task("js", () => {
+gulp.task('js', () =>
   // Buscamos los ficheros JS que necesitamos y los propios
-  return gulp
+  gulp
     .src([
       // "node_modules/bootstrap/dist/js/bootstrap.js",
       // "node_modules/jquery/dist/jquery.js",
       // "node_modules/popper.js/dist/umd/popper.js",
-      "src/scripts/*.js",
+      'src/scripts/*.js',
     ])
     .pipe(
       minify({
         noSource: true,
-        ignoreFiles: [".combo.js", ".min.js"],
-      })
+        ignoreFiles: ['.combo.js', '.min.js'],
+      }),
     )
-    .pipe(gulp.dest("src/js"))
-    .pipe(browserSync.stream());
-});
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream()));
 
 // Servidor para detectar los cambios solo en sass o sass y js
 // gulp.task("serve", gulp.parallel('sass', 'js'), () => {
-gulp.task('serve', gulp.series('sass', function (done) {
+gulp.task('serve', gulp.series('sass', (done) => {
   // indicamos donde debe iniciarse
   browserSync.init({
-    server: "./src"
+    server: './src',
   });
 
   // Detecta cambios y ejecuta la tarea para scss
@@ -53,40 +52,36 @@ gulp.task('serve', gulp.series('sass', function (done) {
     [
       // "node_modules/bootstrap/scss/bootstrap.min.scss",
       // "node_modules/bootstrap/scss/bootstrap.scss",
-      "src/scss/*.scss",
+      'src/scss/*.scss',
     ],
-    gulp.series('sass')
+    gulp.series('sass'),
   );
 
   // Detecta cambios y ejecuta la tarea para mis scripts js, opcional
   gulp.watch(
     [
-      "src/scripts/*.js"
+      'src/scripts/*.js',
     ],
-    gulp.series('js')
+    gulp.series('js'),
   );
 
   // si hay cambios en html...
-  gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
   done();
 }));
 
 // Tarea para obtener recursos
 // iconos
-gulp.task("font-awesome", () => {
-  return gulp
-    .src("node_modules/font-awesome/css/font-awesome.min.css")
-    .pipe(gulp.dest("src/css"));
-});
+gulp.task('font-awesome', () => gulp
+  .src('node_modules/font-awesome/css/font-awesome.min.css')
+  .pipe(gulp.dest('src/css')));
 
 // fuentes
-gulp.task("fonts", () => {
-  return (
-    gulp
-      .src("node_modules/font-awesome/fonts/*")
-      .pipe(gulp.dest("src/fonts"))
-  );
-});
+gulp.task('fonts', () => (
+  gulp
+    .src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('src/fonts'))
+));
 
 // tareas por defecto
-gulp.task("default", gulp.series(["js", "font-awesome", "fonts", "serve"]));
+gulp.task('default', gulp.series(['js', 'font-awesome', 'fonts', 'serve']));
